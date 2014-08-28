7e6d9e792de0366107fcbbaa16b71d60cb5a8ef3
Gabriel Gonzalez <Gabriel439@gmail.com>
2014-08-28 03:52:21 -0700

    Revert "Added support for interleaving effects with updates"
    This reverts commit 42d94334535ad9c9cbaab77f62d223a9f8aaa06d.

--- src/MVC/Updates.hs
+++ src/MVC/Updates.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
+{-# LANGUAGE ExistentialQuantification #-}
 
 {-| Use this library to build @mvc@ applications that consume many individually
     `Updatable` values, such as:
@@ -16,26 +16,33 @@
 
 > import Control.Applicative ((<$>), (<*>))
 > import Control.Foldl (last, length)
-> import MVC.Updates (Updatable, on, listen, runUpdatable)
+> import MVC
+> import MVC.Updates
 > import MVC.Prelude (stdinLines, tick)
+> import qualified Pipes.Prelude as Pipes
 > import Prelude hiding (last, length)
->
+> 
 > data Example = Example (Maybe String) Int deriving (Show)
 >
-> debug :: Show a => String -> Updatable a -> Updatable a
-> debug label = listen (\x -> putStrLn (label ++ ": " ++ show x))
->
 > lastLine :: Updatable (Maybe String)
-> lastLine = debug "lastLine" (on last stdinLines)
+> lastLine = On last stdinLines
 >
-> seconds  :: Updatable Int
-> seconds  = debug "seconds " (on length (tick 1.0))
+> seconds :: Updatable Int
+> seconds = On length (tick 1.0)
 >
-> example  :: Updatable Example
-> example  = debug "example " (Example <$> lastLine <*> seconds)
->
+> example :: Updatable Example
+> example = Example <$> lastLine <*> seconds
+> 
+> viewController :: Managed (View Example, Controller Example)
+> viewController = do
+>     controller <- updates Unbounded example
+>     return (asSink print, controller)
+> 
+> model :: Model () Example Example
+> model = asPipe $ Pipes.takeWhile (\(Example str _) -> str /= Just "quit")
+> 
 > main :: IO ()
-> main = runUpdatable example
+> main = runMVC () model viewController
 
     First we build two simple `Updatable` values:
 
@@ -72,9 +79,6 @@ module MVC.Updates (
     -- * Updates
       -- $updates
       Updatable(..)
-    , on
-    , listen
-    , runUpdatable
     , updates
 
     -- * Example
@@ -86,13 +90,11 @@ module MVC.Updates (
     ) where
 
 import Control.Applicative (Applicative(pure, (<*>)), (<*))
-import Control.Category (id)
 import Control.Concurrent.Async (withAsync)
-import Control.Foldl (FoldM(..), Fold(..))
-import qualified Control.Foldl as Foldl
-import Data.IORef (newIORef, readIORef, writeIORef)
+import Control.Foldl (Fold(..))
+import Control.Monad (forever)
+import Control.Monad.Trans.State.Strict (get, put)
 import MVC
-import Prelude hiding (id)
 
 {- $updates
     You can combine smaller updates into larger updates using `Applicative`
@@ -123,7 +125,7 @@ import Prelude hiding (id)
 -}
 
 -- | A concurrent, updatable value
-data Updatable a = forall e . On (FoldM IO e a) (Managed (Controller e))
+data Updatable a = forall e . On (Fold e a) (Managed (Controller e))
 
 instance Functor Updatable where
     fmap f (On fold mController) = On (fmap f fold) mController
@@ -133,22 +135,22 @@ instance Functor Updatable where
 >
 > onLeft (pure r) = pure r
 -}
-onLeft :: Monad m => FoldM m a b -> FoldM m (Either a x) b
-onLeft (FoldM step begin done) = FoldM step' begin done
+onLeft :: Fold a b -> Fold (Either a x) b
+onLeft (Fold step begin done) = Fold step' begin done
   where
     step' x (Left a) = step x a
-    step' x  _       = return x
+    step' x  _       = x
 
 {-
 > onRight (f <*> x) = onRight f <*> onRight x
 >
 > onRight (pure r) = pure r
 -}
-onRight :: Monad m => FoldM m a b -> FoldM m (Either x a) b
-onRight (FoldM step begin done) = FoldM step' begin done
+onRight :: Fold a b -> Fold (Either x a) b
+onRight (Fold step begin done) = Fold step' begin done
   where
     step' x (Right a) = step x a
-    step' x  _        = return x
+    step' x  _        = x
 
 instance Applicative Updatable where
     pure a = On (pure a) mempty
@@ -160,84 +162,30 @@ instance Applicative Updatable where
         mControllerT =
             fmap (fmap Left) mControllerL <> fmap (fmap Right) mControllerR
 
--- | Create an `Updatable` value using a pure `Fold`
-on :: Fold e a -> Managed (Controller e) -> Updatable a
-on fold = On (Foldl.generalize fold)
-{-# INLINABLE on #-}
-
-{-| Attach a listener that runs every time an `Updatable` value updates
-
-> -- Treating `a -> IO ()` as the `View a` `Monoid`:
->
-> listen mempty = id
->
-> listen (f <> g) = listen g . listen f
--}
-listen :: (a -> IO ()) -> Updatable a -> Updatable a
-listen handler (On (FoldM step begin done) mController) =
-    On (FoldM step' begin' done) mController
-  where
-    begin' = do
-        x <- begin
-        b <- done x
-        handler b
-        return x
-    step' x a = do
-        x' <- step x a
-        b  <- done x'
-        handler b
-        return x'
-{-# INLINABLE listen #-}
-
-{-| Run an `Updatable` value, discarding the result
-
-    Use this if you only care about running the associated listeners
--}
-runUpdatable :: Updatable a -> IO ()
-runUpdatable (On (FoldM step begin done) mController) = runMVC () id $ do
-    controller <- mController
-
-    ioref <- liftIO $ do
-        x     <- begin
-        _     <- done x
-        newIORef x
-
-    let view = asSink $ \e -> do
-            x  <- readIORef ioref
-            x' <- step x e
-            _  <- done x'
-            writeIORef ioref x'
-
-    return (view, controller)
-{-# INLINABLE runUpdatable #-}
-
 {-| Convert an `Updatable` value to a `Managed` `Controller` that emits updates
 
     You must specify how to `Buffer` the updates
 -}
 updates :: Buffer a -> Updatable a -> Managed (Controller a)
-updates buffer (On (FoldM step begin done) mController) = do
+updates buffer (On (Fold step begin done) mController) = do
     controller <- mController
     managed $ \k -> do
         (o, i, seal) <- spawn' buffer
-
-        ioref <- liftIO $ do
-            x <- begin
-            a <- done x
-            _ <- atomically $ send o a
-            newIORef x
-
-        let view = asSink $ \e -> do
-                x  <- readIORef ioref
-                x' <- step x e
-                a  <- done x'
-                _  <- atomically $ send o a
-                writeIORef ioref x'
-
+    
+        let model_ = asPipe $ forever $ do
+                x <- lift get
+                yield (done x)
+                e <- await
+                lift $ put $! step x e
+    
+            view_ = asSink $ \a -> do
+                _ <- atomically (send o a)
+                return ()
+    
         let io = do
-                _ <- runMVC begin id (pure (view, controller))
+                _ <- runMVC begin model_ (pure (view_, controller))
                 atomically seal
-
+    
         withAsync io $ \_ -> k (asInput i) <* atomically seal
 
 -- $example
@@ -257,13 +205,13 @@ updates buffer (On (FoldM step begin done) mController) = do
 -- > import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
 -- > import Control.Concurrent.Async (async, wait)
 -- > import Control.Foldl (lastDef)
--- > import Graphics.UI.Gtk as GTK
+-- > import Graphics.UI.Gtk
 -- > import Lens.Family.TH (makeLenses)
 -- > import MVC
--- > import MVC.Updates as MVC
+-- > import MVC.Updates
 -- > 
 -- > makeInCell :: VBox -> Updatable Double
--- > makeInCell vBox = MVC.on (lastDef 0) $ managed $ \k -> do
+-- > makeInCell vBox = On (lastDef 0) $ managed $ \k -> do
 -- >     (output, input) <- spawn Unbounded
 -- >     spinButton <- spinButtonNewWithRange 0 100 1
 -- >     onValueSpinned spinButton $ do
@@ -295,7 +243,7 @@ updates buffer (On (FoldM step begin done) mController) = do
 -- >     a    <- async $ k (makeInCell vBoxL, makeOutCell vBoxR, putMVar mvar ())
 -- >     takeMVar mvar
 -- > 
--- >     GTK.on window deleteEvent $ do
+-- >     on window deleteEvent $ do
 -- >         liftIO mainQuit
 -- >         return False
 -- >     widgetShowAll window
@@ -341,5 +289,5 @@ updates buffer (On (FoldM step begin done) mController) = do
 --     will automatically update all output cells.
 
 {- $reexports
-    "Control.Foldl" re-exports the `Fold` and `FoldM` types
+    "Control.Foldl" re-exports the `Fold` type
 -}
